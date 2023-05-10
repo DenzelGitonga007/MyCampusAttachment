61a3067a0762d0b9deb1b6e3d0a5e9e90f8d0afb
DenzelGitonga007 <gitongadenzel@gmail.com>
2023-05-10 14:32:25 +0300

    chat models

--- django_chat_app/chat/models.py
+++ django_chat_app/chat/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+# Chat and message models
+
+class Message(models.Model):
+    text = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=TRUE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+class Chat(models.Model):
+    users = models.ManyToManyField(User, related_name='chats')
+    messages = models.ManyToManyField(Message)
