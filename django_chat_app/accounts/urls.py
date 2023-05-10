ff0034069206378fe16f74b83290b587c68ebbab
DenzelGitonga007 <gitongadenzel@gmail.com>
2023-05-10 15:47:29 +0300

    accounts urls

---
+++ django_chat_app/accounts/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+
+
+urlpatterns = [
+    path('', views.home, name='home'), # home view/ landing page
+    path('register/', views.register_user, name='register'), #registration form
+    path('login/', views.login_user, name='login'), #login page
+    path('logout/', views.logout_user, name='logout'), #logout page
+]
