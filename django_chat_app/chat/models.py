745d8e9c1188447c43266549f5b60f5b326f3fec
DenzelGitonga007 <gitongadenzel@gmail.com>
2023-05-10 15:52:37 +0300

    change User

--- django_chat_app/chat/models.py
+++ django_chat_app/chat/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+# Import the settings
+from django.conf import settings
 
 # Create your models here.
 # Chat and message models
@@ -7,8 +9,8 @@ from django.contrib.auth.models import User
 class Message(models.Model):
     text = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
 class Chat(models.Model):
-    users = models.ManyToManyField(User, related_name='chats')
+    users = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='chats')
     messages = models.ManyToManyField(Message)
