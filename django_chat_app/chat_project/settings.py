92dc0320653ef6751309b12d8793325263f930bd
DenzelGitonga007 <gitongadenzel@gmail.com>
2023-05-11 14:22:08 +0300

    static files

--- django_chat_app/chat_project/settings.py
+++ django_chat_app/chat_project/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 
+import os
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -128,3 +130,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'accounts.User'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
+
+# For the static folder, and styling
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
