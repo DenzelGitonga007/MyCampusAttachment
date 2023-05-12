6fc421e6b2acc0fdb8f98d17e20acb0997bd4af2
DenzelGitonga007 <gitongadenzel@gmail.com>
2023-05-12 15:54:53 +0300

    chat with bot view

--- django_chat_app/chat/views.py
+++ django_chat_app/chat/views.py
@@ -25,23 +25,54 @@ def create_chat(request):
     return render(request, 'chat/create_chat.html', context)
 
 # Chat view
+
+
+
+# Chat with bot
 @login_required
-# Chat with someone
-# def chat_view(request, chat_id):
-#     chat = get_object_or_404(Chat, id=chat_id, users=request.user)
-#     messages = Message.objects.filter(chat=chat).order_by('timestamp')
-#     if request.method == 'POST':
-#         form = MessageForm(request.POST)
-#         if form.is_valid():
-#             message = form.save(commit=False)
-#             message.chat = chat
-#             message.sender = request.user
-#             message.save()
-#             return redirect('chat_view', chat_id=chat.id)
-#     else:
-#         form = MessageForm()
-#     context = {'chat': chat, 'messages': messages, 'form': form}
-#     return render(request, 'chat/chat_view.html', context)
+def chat_view(request):
+    if request.method == 'POST':
+        message = request.POST['message']
+        ChatMessage.objects.create(sender=request.user, content=message)
+        #TODO: Generate chat bot response and save
+        # Bot response
+        bot_response = generate_response(message)
+        # Save the message even for the bot
+        bot_message = ChatMessage.objects.create(content=bot_response, sender=None)
+    messages = ChatMessage.objects.order_by('timestamp')[:50]
+    context = {'messages': messages}
+    return render(request, 'chatbot/chat.html', context)
+
+# Generate responses
+def generate_response(message):
+    keywords = ['hi', 'hello', 'hi']
+    # Check if the messsage has any of the keyword
+    if message.lower() in keywords:
+        return "Hello human"
+    else:
+        return "Sorry, I can only understand and respond to greetings now"
+
+
+
+
+# @login_required
+# # Chat with someone
+# # def chat_view(request, chat_id):
+# #     chat = get_object_or_404(Chat, id=chat_id, users=request.user)
+# #     messages = Message.objects.filter(chat=chat).order_by('timestamp')
+# #     if request.method == 'POST':
+# #         form = MessageForm(request.POST)
+# #         if form.is_valid():
+# #             message = form.save(commit=False)
+# #             message.chat = chat
+# #             message.sender = request.user
+# #             message.save()
+# #             return redirect('chat_view', chat_id=chat.id)
+# #     else:
+# #         form = MessageForm()
+# #     context = {'chat': chat, 'messages': messages, 'form': form}
+# #     return render(request, 'chat/chat_view.html', context)
+
 
 # Sending the message
 @login_required
@@ -61,24 +92,3 @@ def send_message(request, chat_id):
         messages.error(request, 'Invalid request method.')
         # return redirect('chat_list')
         return redirect('chat_view')
-
-
-# Chat with bot
-@login_required
-def chat_view(request):
-    if request.method == 'POST':
-        message = request.POST['message']
-        ChatMessage.objects.create(sender=request.user, content=message)
-        #TODO: Generate chat bot response and save
-    messages = ChatMessage.objects.order_by('timestamp')[:50]
-    context = {'messages': messages}
-    return render(request, 'chatbot/chat.html', context)
-
-# Generate responses
-def generate_response(request):
-    keywords = ['hi', 'hello', 'hi']
-    # Check if the messsage has any of the keyword
-    if message.lower() in keywords:
-        return "Hello human"
-    else:
-        return "Sorry, I can only understand and respond to greetings now"
