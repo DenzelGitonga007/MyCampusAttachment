086b0fc31d9119921b5b7da41a0b0d4ceea6f8fb
DenzelGitonga007 <gitongadenzel@gmail.com>
2023-05-12 15:20:25 +0300

    chatbot view

--- django_chat_app/chat/views.py
+++ django_chat_app/chat/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
-from .models import Chat, Message
+from .models import Chat, Message, ChatMessage
 from .forms import UserChoiceForm, MessageForm
 
 
@@ -26,21 +26,22 @@ def create_chat(request):
 
 # Chat view
 @login_required
-def chat_view(request, chat_id):
-    chat = get_object_or_404(Chat, id=chat_id, users=request.user)
-    messages = Message.objects.filter(chat=chat).order_by('timestamp')
-    if request.method == 'POST':
-        form = MessageForm(request.POST)
-        if form.is_valid():
-            message = form.save(commit=False)
-            message.chat = chat
-            message.sender = request.user
-            message.save()
-            return redirect('chat_view', chat_id=chat.id)
-    else:
-        form = MessageForm()
-    context = {'chat': chat, 'messages': messages, 'form': form}
-    return render(request, 'chat/chat_view.html', context)
+# Chat with someone
+# def chat_view(request, chat_id):
+#     chat = get_object_or_404(Chat, id=chat_id, users=request.user)
+#     messages = Message.objects.filter(chat=chat).order_by('timestamp')
+#     if request.method == 'POST':
+#         form = MessageForm(request.POST)
+#         if form.is_valid():
+#             message = form.save(commit=False)
+#             message.chat = chat
+#             message.sender = request.user
+#             message.save()
+#             return redirect('chat_view', chat_id=chat.id)
+#     else:
+#         form = MessageForm()
+#     context = {'chat': chat, 'messages': messages, 'form': form}
+#     return render(request, 'chat/chat_view.html', context)
 
 # Sending the message
 @login_required
@@ -61,3 +62,14 @@ def send_message(request, chat_id):
         # return redirect('chat_list')
         return redirect('chat_view')
 
+
+# Chat with bot
+@login_required
+def chat_view(request):
+    if request.method == 'POST':
+        message = request.POST['message']
+        ChatMessage.objects.create(sender=request.user, content=message)
+        #TODO: Generate chat bot response and save
+    messages = ChatMessage.objects.order_by('-timestamp')[:50]
+    context = {'messages': messages}
+    return render(request, 'chatbot/chat.html', context)
