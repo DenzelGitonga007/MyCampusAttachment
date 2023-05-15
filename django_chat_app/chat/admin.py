95d99b6557d2f3d2ad055bb78301849e20bd3469
DenzelGitonga007 <gitongadenzel@gmail.com>
2023-05-15 14:09:11 +0300

    deleted the chat app to start afresh

--- django_chat_app/chat/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import ChatMessage
-
-class ChatMessageAdmin(admin.ModelAdmin):
-    list_display = ('sender', 'content')
-admin.site.register(ChatMessage)
--- django_chat_app/chat/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('create_chart/', views.create_chat, name='create_chat'), # create a chat
-    # path('chat/<int:chat_id>/', views.chat_view, name='chat_view'), # launch chat
-    path('chat/', views.chat_view, name='chat'), # chat with bot
-    path('chat/<int:chat_id>/send-message/', views.send_message, name='send_message'),
-]
